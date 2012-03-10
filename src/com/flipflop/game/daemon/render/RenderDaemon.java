11ec24ae44703144d40c0a583f85dadc8429b025
Joseph Gilley <joe.gilley@gmail.com>
2012-03-10 15:17:22 -0500

    Stopped using Display's stupid singlethreaded sync

--- src/com/flipflop/game/daemon/render/RenderDaemon.java
+++ src/com/flipflop/game/daemon/render/RenderDaemon.java
@@ -73,7 +73,7 @@ public class RenderDaemon extends Daemon {
 
 	@Override
 	public void execute() {
-		Display.sync(60);
+		//Display.sync(60);
 		// Clear buffer for redrawing.
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 

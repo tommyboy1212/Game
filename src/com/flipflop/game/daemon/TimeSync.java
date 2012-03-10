acdabf9d12cb85c83fc3bd09e5413e10a8a29157
Joseph Gilley <joe.gilley@gmail.com>
2012-03-10 18:49:28 -0500

    Drastically improved accuracy of time sync by implementing error feedback and execution estimation.

--- src/com/flipflop/game/daemon/TimeSync.java
+++ src/com/flipflop/game/daemon/TimeSync.java
@@ -1,5 +1,7 @@
 package com.flipflop.game.daemon;
 
+import com.flipflop.utility.concurrency.AtomicFloat;
+
 public class TimeSync {
 	public static final long NANO_IN_SECOND = 1000 * 1000 * 1000;
 	public static final long NANO_IN_MILLI = 1000 * 1000;
@@ -7,13 +9,14 @@ public class TimeSync {
 	public static final long MILLI_IN_SECOND= 1000;
 	private long lastTickAtNano = System.nanoTime();
 	private float targetTPS = 60.0f;
-	private float actualTPS = 0.0f;
+	private AtomicFloat actualTPS = new AtomicFloat(0.0f);
 	private long ticks = 0;
 	private float runTime = 0;
-	private long nanoSinceSecond = 0;
+	private long nanoPassed = 0;
 	private long nanoToWait = 0;
 	private boolean isSlow = false;
 	private long nanoErrorFeedback = 0;
+	private long lastNano = System.nanoTime();
 
 	public TimeSync() {
 		setTargetTPS(this.targetTPS);
@@ -35,15 +38,17 @@ public class TimeSync {
 	private void sleep() {
 		long currNano = System.nanoTime();
 		long alarmSet = currNano + this.nanoToWait;
+		long anticipatedExecution = currNano - this.lastNano;
 		try {
-			while ((currNano+this.nanoErrorFeedback) < alarmSet) {
-				Thread.sleep((long)(1.0f/this.targetTPS*MILLI_IN_SECOND));
+			while ((currNano=(currNano+this.nanoErrorFeedback)) < alarmSet) {
+				Thread.sleep(1);
 				currNano = System.nanoTime();
 			}
-			this.nanoErrorFeedback = currNano - alarmSet;
 		} catch (InterruptedException e) {}
 		finally {
+			this.nanoErrorFeedback = currNano - alarmSet + anticipatedExecution ;
 			this.setActualTPS();
+			this.lastNano = System.nanoTime();
 		}
 		this.isSlow = false;
 	}
@@ -54,21 +59,23 @@ public class TimeSync {
 
 	public void setTargetTPS(float tps) {
 		this.targetTPS = tps;
-		this.nanoToWait = (long) (((float) 1 / this.targetTPS) * NANO_IN_MILLI);
+		this.nanoToWait = (long) (((float) 1 / this.targetTPS) * NANO_IN_SECOND);
 	}
 
 	public float getActualTPS() {
-		return this.actualTPS;
+		return this.actualTPS.get();
 	}
 
 	public void setActualTPS() {
 		this.ticks++;
-		this.nanoSinceSecond += System.nanoTime() - this.lastTickAtNano;
-		if (this.nanoSinceSecond >= NANO_IN_SECOND) {
-			this.runTime += (float) this.nanoSinceSecond / NANO_IN_SECOND;
-			this.actualTPS = (float) this.ticks / (this.nanoSinceSecond * NANO_IN_SECOND);
-			this.nanoSinceSecond = 0;
+		this.nanoPassed += System.nanoTime() - this.lastTickAtNano;
+		if (this.nanoPassed >= NANO_IN_SECOND) {
+			this.runTime += (float) this.nanoPassed / NANO_IN_SECOND;
+			this.actualTPS.set((float) this.ticks / this.nanoPassed * NANO_IN_SECOND);
+			this.nanoPassed = 0;
+			this.ticks = 0;
 		}
+		this.lastTickAtNano = System.nanoTime();
 	}
 	
 	public float getRunTime() {
